c19d732f20b85b82605c7a8df214c25c41f61a82
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-03 18:35:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function lowerCaseDrivers(array){
 
 function nameToAttributes(names){
   names.map(function(driver){
-    return drives.firstName drivers.lastName
+    let firstName = name.split(" ")[0]
+    let lastName = name.split(" ")[1]
   })
 }
