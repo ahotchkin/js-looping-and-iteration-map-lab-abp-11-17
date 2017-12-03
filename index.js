75ba0952b1c7563e24ab041943463391e4233041
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-03 18:57:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ function nameToAttributes(names){
 }
 
 function attributesToPhrase(drivers){
-  return drivers.map(function(name, hometown){
+  return drivers.map(function(driver){
     return `${name} is from ${hometown}`
   })
 }
