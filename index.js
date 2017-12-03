ba706496b288d5c9615d5e70d1fc0b76330c9a53
Allyson Hotchkin <ahotchk1@yahoo.com>
2017-12-03 18:11:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(){
+  return Object.assign({}, {drivers.toLowerCase})
+}
+
+// newEngineers.map(function (eng) {
+//   return Object.assign({}, eng, { equipment: 'Laptop' });
+// 	});
